858b02b552ef81418098276be89acc3db3c56dce
Eser <Eser@LAPTOP-EVSNA4JH>
2021-05-03 09:00:31 +0300

    JWT1

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -31,8 +31,8 @@ namespace Business.Concrete
             _categoryService = categoryService;
             
         }
-
-
+        //claim
+        //[SecuredOperation("product.add")]
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
